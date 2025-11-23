eb093ce0888743ceb018060ec83143f9c84e4843
ANGÉL <asa@ANGEL.local>
2025-11-23 10:29:13 -0500

    first commit

---
+++ controllers/posts.js
@@ -0,0 +1,114 @@
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      const posts = await Post.find({ user: req.user.id });
+      res.render("profile.ejs", { posts: posts, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getFeed: async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+      res.render("feed.ejs", { posts: posts });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getPost: async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.render("post.ejs", { post: post, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createPost: async (req, res) => {
+    try {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      console.log(req.body)
+      await Post.create({
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        brand: req.body.brand,
+        company: req.body.company,
+        businessType: req.body.businessType,
+        about: req.body.about,
+        likes: 0,
+        user: req.user.id
+        
+      });
+      console.log("Post has been added!");
+      res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  updatePost: async (req, res) => {
+    try {
+      // Find post by id
+      let oldPost = await Post.findById({ _id: req.params.id });
+      let result = {
+        "secure_url": oldPost.image,
+        "public_id": oldPost.cloudinaryId
+      }
+      if (req.file.path) {
+        // Update image to cloudinary
+        result = await cloudinary.uploader.upload(req.file.path);
+        // Check if the id is existed, Delete image from cloudinary
+        if (oldPost.cloudinaryId) {
+          await cloudinary.uploader.destroy(oldPost.cloudinaryId);
+        }
+      }
+      const post = await Post.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+          image: result.secure_url,
+          cloudinaryId: result.public_id,
+          brand: req.body.brand,
+          company: req.body.company,
+          businessType: req.body.businessType,
+          about: req.body.about
+        },
+      );
+      console.log("Post has been updated!");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+      res.redirect("/profile");
+    }
+  },
+  likePost: async (req, res) => {
+    try {
+      await Post.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deletePost: async (req, res) => {
+    try {
+      // Find post by id
+      let post = await Post.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(post.cloudinaryId);
+      // Delete post from db
+      await Post.remove({ _id: req.params.id });
+      console.log("Deleted Post");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+};
